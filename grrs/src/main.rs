0b24f0916c2ae649761b66511b69e3abf7afaf8e
gabrielberluccidev <gabrielberluccidev@proton.me>
2023-12-20 23:57:13 -0300

    feat: added file reader and verification

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,9 +1,25 @@
+use clap::Parser;
+
+/* Search for a pattern in a file and display the lines */
+#[derive(Parser)]
+struct Cli {
+    // Pattern to look for
+    pattern: String,
+    // Path to the file
+    path: std::path::PathBuf,
+}
+
+
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no patern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", pattern, path);
+    let content = std::fs::read_to_string(&args.path).expect("could not read the file");
 
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 
 
 }
