c9ecf5b50c82095ace795080d8f83eec00c7c0f8
gabrielberluccidev <gabrielberluccidev@proton.me>
2023-12-22 18:42:25 -0300

    feat: implemented a grep-like function

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,4 @@
+
 use clap::Parser;
 
 /* Search for a pattern in a file and display the lines */
@@ -9,17 +10,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-
 fn main() {
     let args = Cli::parse();
-
-    let content = std::fs::read_to_string(&args.path).expect("could not read the file");
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
-
-
 }
