ef0b0f8d71f52e62076ae4edebf74d30e99c285f
gabrielberluccidev <gabrielberluccidev@proton.me>
2024-01-16 11:10:53 -0300

    dependencies

---
+++ grrs/target/debug/build/anyhow-2960675a7d8ac2e1/build_script_build-2960675a7d8ac2e1.d
@@ -0,0 +1,5 @@
+/home/gabrielberlucci/Rust-CLI/grrs/target/debug/build/anyhow-2960675a7d8ac2e1/build_script_build-2960675a7d8ac2e1: /home/gabrielberlucci/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.79/build.rs
+
+/home/gabrielberlucci/Rust-CLI/grrs/target/debug/build/anyhow-2960675a7d8ac2e1/build_script_build-2960675a7d8ac2e1.d: /home/gabrielberlucci/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.79/build.rs
+
+/home/gabrielberlucci/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.79/build.rs:
---
+++ grrs/target/debug/deps/grrs-fee575ad5fd2791d.d
@@ -0,0 +1,5 @@
+/home/gabrielberlucci/Rust-CLI/grrs/target/debug/deps/grrs-fee575ad5fd2791d: src/main.rs
+
+/home/gabrielberlucci/Rust-CLI/grrs/target/debug/deps/grrs-fee575ad5fd2791d.d: src/main.rs
+
+src/main.rs:
